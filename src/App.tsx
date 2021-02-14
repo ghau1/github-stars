783e6fe724cb327e1aefba3787ebf175871a98ce
Ben Clark <ben.clark@appsody.co.uk>
2021-02-14 13:12:24 +0000

    Set up grahpql connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Header } from 'antd/lib/layout/layout';
 
 import './App.css';
 
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-
+    <ApolloProvider client={client}>
       <Header>
         <h1 className="Header-Title">
           Github Stars
         </h1>
       </Header>
+    </ApolloProvider>
   );
 }
 
